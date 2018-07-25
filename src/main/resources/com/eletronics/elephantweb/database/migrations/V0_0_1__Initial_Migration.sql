e484c6e7bee0b5e83f8fa2f3fdff963c54776703
Nissanka <contact.eletronics@gmail.com>
2018-07-25 20:13:10 +1200

    Use mariadb for compile db and get web server working to allow changing config table entries YAY!

--- src/main/resources/com/eletronics/elephantweb/database/migrations/V0_0_1__Initial_Migration.sql
+++ src/main/resources/com/eletronics/elephantweb/database/migrations/V0_0_1__Initial_Migration.sql
@@ -1,9 +1,14 @@
 CREATE TABLE configuration (
-  id          INTEGER PRIMARY KEY AUTOINCREMENT,
-  name        VARCHAR NOT NULL UNIQUE,
-  value       TEXT,
-  editable    BOOLEAN
-);
+  id          int(11)      NOT NULL AUTO_INCREMENT,
+  name        varchar(100) NOT NULL,
+  value       varchar(300),
+  description varchar(300)          DEFAULT NULL,
+  editable    BOOLEAN               DEFAULT TRUE,
+  PRIMARY KEY (id),
+  UNIQUE KEY name_UNIQUE (name)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 
-INSERT INTO configuration (name, value, editable) VALUES
- ('version', '1.0.0', 0);
+INSERT INTO configuration (name, value, description, editable) VALUES
+  ('version', '1.0.0', 'Application version', 0);
